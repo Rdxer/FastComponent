8d19f5ab203bfd221f2a6cffb61682b091f4e21a
LXF <Rdxer@foxmail.com>
2018-09-17 10:42:31 +0800

    🎨

--- Base/NSObject+Ex+Tag.swift
+++ Base/NSObject+Ex+Tag.swift
@@ -9,16 +9,16 @@
 import Foundation
 import FastComponent
 
-private var XX_Ex_Tag_Key: Void?
+private var XX_Ex_Tag_Key: String = "FastComponent.XX_Ex_Tag_Key"
 public extension XXExtension where Base:NSObject{
     
     /// 附属物品  - RETAIN_NONATOMIC
     public var tag:Any? {
         get {
-            return objc_getAssociatedObject(self, &XX_Ex_Tag_Key)
+            return objc_getAssociatedObject(self.base, &XX_Ex_Tag_Key)
         }
         set {
-            objc_setAssociatedObject(self, &XX_Ex_Tag_Key, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            objc_setAssociatedObject(self.base, &XX_Ex_Tag_Key, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
         }
     }
 }
